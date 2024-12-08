db78e3739b7aae3b31f35605c86861fe60417ee9
Javier <japerez@nevada.unr.edu>
2024-12-07 22:07:41 -0800

    Added method for candle requests and started analyzing data for display goals

--- Coinbase.py
+++ Coinbase.py
@@ -1,37 +1,49 @@
 from coinbase.rest import RESTClient
 from dotenv import dotenv_values
-from json import dumps
+from portfolio import Granularity
+import utils
+import datetime
 
+# Load environment variables
 config = dotenv_values(".env")
 coinbaseAPIKey = config["COINBASE_API_KEY"]
 coinbaseAPISecret = config["COINBASE_API_SECRET"]
 
 client = RESTClient(api_key=coinbaseAPIKey, api_secret=coinbaseAPISecret)
 
-portfolios_response = client.get_portfolios()
-portfolios = portfolios_response.to_dict()["portfolios"]
-print(dumps(portfolios, indent=2))
+def get_default_portfolio():
+    portfolios_response = client.get_portfolios()
+    portfolios = portfolios_response.to_dict()["portfolios"]
 
-default_portfolio = None
-for portfolio in portfolios:
-    portfolio_bd_response = client.get_portfolio_breakdown(portfolio_uuid=portfolio["uuid"])
-    portfolio_bd = portfolio_bd_response.to_dict()
-    #print(dumps(portfolio_bd, indent=2))
+    for portfolio in portfolios:
+        portfolio_bd_res = client.get_portfolio_breakdown(portfolio_uuid=portfolio["uuid"])
+        portfolio_bd = portfolio_bd_res.to_dict()["breakdown"]
+        #timestamp = int(datetime.datetime.now().timestamp())
+        #utils.write_data_to_file(utils.get_path_from_cwd(f"portfolios_{timestamp}.json"), portfolio_bd)
 
-    if portfolio["type"] == "DEFAULT":
-        default_portfolio = portfolio_bd["breakdown"]
+        if portfolio["type"] == "DEFAULT":
+            default_portfolio = portfolio_bd
+    return default_portfolio
+ 
+def get_asset_candles(product_id: str, granularity: str, start: datetime.datetime, end: datetime.datetime, limit= int | None):
+    start_unix = str(int(start.timestamp()))
+    end_unix = str(int(end.timestamp()))
+    if (not Granularity.verify(granularity)):
+        raise ValueError("Granularity must be one of the following: ONE_MINUTE, FIVE_MINUTES, FIFTEEN_MINUTES, THIRTY_MINUTES, ONE_HOUR, TWO_HOUR, SIX_HOUR, ONE_DAY")
+    candles_response = client.get_candles(product_id=product_id, start=start_unix, end=end_unix, granularity=granularity, limit=limit)
+    candles = candles_response.to_dict()["candles"]
+    # timestamp = int(datetime.datetime.now().timestamp())
+    # utils.write_data_to_file(utils.get_path_from_cwd(f"{product_id}_candles_{timestamp}.json"), candles)
+    return candles
 
+def main():
+    candles = utils.get_data_from_file(utils.get_path_from_log_dir("candles.json"))["candles"]
 
+    for candle in candles:
+        date: str = utils.unix_to_date_string(int(candle["start"]))
+        high: str = candle["high"]
+        weekday: str = utils.get_weekday(datetime.datetime.fromtimestamp(int(candle["start"])))
+        print(f"{date} {weekday}: {high}")
 
-print()
-
-spot_positions = default_portfolio["spot_positions"]
-print("Positions:")
-# TODO: import panda and format data into table
-for position in spot_positions:
-    asset_name: str = position["asset"]
-    curr_value: float = float(position["cost_basis"]["value"])
-    value_currency: str = position["cost_basis"]["currency"]
-    amount: float = position["total_balance_fiat"]
-    print(asset_name, curr_value, value_currency, amount, sep='\t')
-print()
+if __name__=="__main__":
+    main()
