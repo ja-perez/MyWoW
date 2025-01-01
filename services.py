9faee17b6fc761a5c8b152a5ac5ff71644283b0f
Javier <japerez@nevada.unr.edu>
2024-12-31 21:21:28 -0800

    Updated candles method data processing and formatted data before uploading in add prediciton

--- services.py
+++ services.py
@@ -17,7 +17,7 @@ class PredictionService:
         for row in file_data:
             data.append({
                 "symbol": row[0],
-                "trading-pair": row[1],
+                "trading_pair": row[1],
                 "start_date": row[2],
                 "end_date": row[3],
                 "start_price": float(row[4]),
@@ -33,7 +33,7 @@ class PredictionService:
         for row in file_data:
             data.append({
                 "symbol": row[0],
-                "trading-pair": row[1],
+                "trading_pair": row[1],
                 "start_date": row[2],
                 "end_date": row[3],
                 "start_price": float(row[4]),
@@ -53,7 +53,7 @@ class PredictionService:
             end_date = datetime.datetime.strptime(pred["end_date"], "%Y-%m-%d")
             start_date = datetime.datetime.strptime(pred["start_date"], "%Y-%m-%d")
             if todays_date > end_date.date():   # want to update preds day after end date
-                trading_pair = pred["trading-pair"]
+                trading_pair = pred["trading_pair"]
                 candle = cb.get_asset_candles(self.client, trading_pair, Granularity.ONE_DAY, start_date, end_date) 
                 pred_result = pred.copy()
                 pred_result["actual_end_price"] = float(candle[0]["close"])
@@ -63,9 +63,19 @@ class PredictionService:
         utils.write_many_data_to_csv_file(self.prediction_data_path, new_data)
 
     def add_prediction(self, prediction: dict):
-        utils.add_data_to_csv_file(self.prediction_data_path, prediction)
+        ordered_prediction = {
+            "symbol": prediction["symbol"],
+            "trading_pair": prediction["trading_pair"],
+            "start_date": prediction["start_date"],
+            "end_date": prediction["end_date"],
+            "start_price": prediction["start_price"],
+            "end_price": prediction["end_price"],
+            "buy_price": prediction["buy_price"],
+            "sell_price": prediction["sell_price"],
+        }
+        utils.add_data_to_csv_file(self.prediction_data_path, ordered_prediction)
 
-    def edit_prediction(self, symbol: str, start_date: str):
+    def remove_prediction(self, symbol: str, start_date: str):
         file_data = self.get_predictions()
 
         new_data = []
@@ -77,7 +87,7 @@ class PredictionService:
         utils.write_many_data_to_csv_file(self.prediction_data_path, new_data)
 
     def get_candles(self, trading_pair: str, start_date: datetime.datetime, end_date: datetime.datetime) -> list[dict]:
-        output_filename = f"{start_date.strftime("%Y%m%d")}_{end_date.strftime("%Y%m%d")}_{trading_pair}_candles.json"
+        output_filename = f'{start_date.strftime("%Y%m%d")}_{end_date.strftime("%Y%m%d")}_{trading_pair}_candles.json'
         output_file_path = utils.get_path_from_data_dir(output_filename)
 
         if os.path.exists(output_file_path):
@@ -85,6 +95,27 @@ class PredictionService:
         else:
             candles = cb.get_asset_candles(self.client, trading_pair, Granularity.ONE_DAY, start_date, end_date)
             utils.write_dict_data_to_file(output_file_path, candles)
+
+        range_high = float('-inf')
+        range_low = float('inf')
+        for candle in candles:
+            range_high = max(range_high, float(candle['high']))
+            range_low = min(range_low, float(candle['low']))
+
+        for i, candle in enumerate(candles):
+            candle = {
+                'start': int(candle['start']),
+                'low': float(candle['low']),
+                'high': float(candle['high']),
+                'open': float(candle['open']),
+                'close': float(candle['close']),
+                'volume': int(candle['volume']),
+            }
+            candle['date'] = utils.unix_to_datetime_string(candle['start'])
+            candle['range_high'] = range_high
+            candle['range_low'] = range_low
+
+            candles[i] = candle
 
         return candles
 
