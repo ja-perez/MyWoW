9f45a0a8f3e3c300b9e32b986d5d95f40db8b937
Javier <japerez@nevada.unr.edu>
2025-01-11 18:59:54 -0800

    Basic functionality of portfolio model and service done

--- models/portfolio.py
+++ models/portfolio.py
@@ -1,9 +1,21 @@
 from coinbase.rest import RESTClient # type: ignore
 
+class Position:
+    def __init__(self, data: dict):
+        self.symbol = data['asset']
+        self.quantity = data['total_balance_crypto']
+        self.value = data['total_balance_fiat']
+
+        self.curr_price = self.value / self.quantity
+
+        self.entry_value = data['average_entry_price']['value']
+        self.entry_cost = data['cost_basis']['value']
+
 class Portfolio:
     def __init__(self, data: dict):
         self.data: dict = data
         self.balance: dict = self.get_balance()
+        self.active_positions = [Position(position) for position in self.data['spot_positions']]
 
     def get_balance(self):
         balance = self.data['portfolio_balances']
--- services/portfolio_service.py
+++ services/portfolio_service.py
@@ -1,15 +1,30 @@
-import services.coinbase as cb
-from coinbase.rest import RESTClient # type: ignore
+import datetime
 from typing import Optional
+from coinbase.rest import RESTClient # type: ignore
+from os import path
 
+import services.coinbase as cb
 from database import Database
+from models.portfolio import Portfolio
+import utils.utils
 
 class PortfolioService:
     def __init__(self, client: RESTClient = None, db: Optional[Database] = None):
         self.client = client if client else cb.get_client()
         self.db = db if db else Database()
 
-    def get_portfolio(self):
-        default_portfolio = cb.get_default_portfolio(self.client)
+        self.cache_path = utils.get_path_from_data_dir('portfolios.json')
 
-        return default_portfolio
+    def get_portfolio(self):
+        if path.exists(self.cache_path):
+            file_data = utils.get_dict_data_from_file(self.cache_path)
+
+            last_updated = datetime.datetime.strptime(file_data['last_updated'], '%Y-%m-%d')
+            if last_updated.date() == datetime.date.today():
+                return Portfolio(file_data)
+
+        default_portfolio = cb.get_default_portfolio(self.client)
+        default_portfolio['last_updated'] = datetime.date.today().strftime('%Y-%m-%d')
+        utils.write_dict_data_to_file(self.cache_path, default_portfolio) 
+        portfolio = Portfolio(default_portfolio)
+        return portfolio
