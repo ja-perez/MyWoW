8198d2230437fd32abbe96deb417438d5f3ff83e
Javier <japerez@nevada.unr.edu>
2025-02-08 07:29:28 -0800

    updated granularity class with helper methods for calculating time differences, fixed candles getter breaking for certain start and end times, and added gettters for market trades and candles for those market trades

--- services/coinbase_services.py
+++ services/coinbase_services.py
@@ -1,24 +1,49 @@
 from coinbase.rest import RESTClient # type: ignore
 from coinbase.wallet.client import Client # type: ignore
+from typing import Optional
 from dotenv import dotenv_values
-import utils
+from math import ceil
 import datetime
+
+CANDLES_LIMIT_MAX = 350
 
 class Granularity:
     ONE_MINUTE = 'ONE_MINUTE'
     FIVE_MINUTES = 'FIVE_MINUTES'
     FIFTEEN_MINUTES = 'FIFTEEN_MINUTES'
     THIRTY_MINUTES = 'THIRTY_MINUTES'
-    ONE_HOU = 'ONE_HOUR'
+    ONE_HOUR = 'ONE_HOUR'
     TWO_HOUR = 'TWO_HOUR'
     SIX_HOUR = 'SIX_HOUR'
     ONE_DAY = 'ONE_DAY'
 
+    seconds = {
+        ONE_MINUTE: 60,
+        FIVE_MINUTES: 5 * 60,
+        FIFTEEN_MINUTES: 15 * 60,
+        THIRTY_MINUTES: 30 * 60,
+        ONE_HOUR: 3600,
+        TWO_HOUR: 2 * 3600,
+        SIX_HOUR: 6 * 3600,
+        ONE_DAY: 24 * 3600
+    }
+
     @staticmethod
     def verify(granularity: str) -> bool:
         return granularity in [Granularity.ONE_MINUTE, Granularity.FIVE_MINUTES, Granularity.FIFTEEN_MINUTES,
-                              Granularity.THIRTY_MINUTES, Granularity.ONE_HOU, Granularity.TWO_HOUR, Granularity.SIX_HOUR,
+                              Granularity.THIRTY_MINUTES, Granularity.ONE_HOUR, Granularity.TWO_HOUR, Granularity.SIX_HOUR,
                               Granularity.ONE_DAY]
+
+    @staticmethod
+    def to_seconds(granularity: str) -> int:
+        return Granularity.seconds[granularity]
+    
+    @staticmethod
+    def get_count_from_granularity(start: datetime.datetime, end: datetime.datetime, granularity: str) -> int:
+        time_delta = end - start
+        time_delta_seconds = time_delta.seconds + time_delta.days * 24 * 3600
+
+        return ceil(time_delta_seconds / Granularity.to_seconds(granularity=granularity))
 
 def get_client(dotenv_path: str = ".env") -> RESTClient:
     # Load environment variables
@@ -52,32 +77,56 @@ def get_default_portfolio(client: RESTClient):
             default_portfolio = portfolio_bd
     return default_portfolio
  
-def get_asset_candles(client: RESTClient, product_id: str, granularity: str, start: datetime.datetime, end: datetime.datetime, limit: int | None = None):
-    start_unix = str(int(start.timestamp()))
-    end_unix = str(int(end.timestamp()))
+def get_asset_candles(client: RESTClient, product_id: str, granularity: str, start: datetime.datetime, end: datetime.datetime, limit: int = CANDLES_LIMIT_MAX):
     if (not Granularity.verify(granularity)):
         raise ValueError("Granularity must be one of the following: ONE_MINUTE, FIVE_MINUTES, FIFTEEN_MINUTES, THIRTY_MINUTES, ONE_HOUR, TWO_HOUR, SIX_HOUR, ONE_DAY")
-    candles_response = client.get_candles(product_id=product_id, start=start_unix, end=end_unix, granularity=granularity, limit=limit)
-    candles = candles_response.to_dict()["candles"]
-    for candle in candles:
-        candle['trading_pair'] = product_id
-    # timestamp = int(datetime.datetime.now().timestamp())
+    candles: list = []
+    base_end_time = end + datetime.timedelta(seconds=1)
+    while True:
+        if Granularity.get_count_from_granularity(start, end, granularity) > CANDLES_LIMIT_MAX:
+            end = min(start + datetime.timedelta(seconds=Granularity.to_seconds(granularity) * CANDLES_LIMIT_MAX), base_end_time)
+
+        start_unix = str(int(start.timestamp()))
+        end_unix = str(int(end.timestamp()))
+
+        res = client.get_candles(product_id=product_id, start=start_unix, end=end_unix, granularity=granularity, limit=None)
+        curr_candles = res.to_dict()['candles']
+        for candle in curr_candles:
+            candle['trading_pair'] = product_id
+        candles = curr_candles + candles 
+
+        if Granularity.get_count_from_granularity(start, end, granularity) < limit:
+            break
+        else:
+            start = end
+            end = base_end_time
+
     # utils.write_data_to_file(utils.get_path_from_cwd(f"{product_id}_candles_{timestamp}.json"), candles)
+    return candles
+
+def fetch_market_trades(client: RESTClient, product_id: str, start_time: datetime.datetime, end_time: datetime.datetime, limit: int):
+    market_trades: list = []
+    while start_time < end_time:
+        start_unix = str(int(start_time.timestamp()))
+        end_unix = str(int(end_time.timestamp()))
+
+        res = client.get_market_trades(product_id=product_id, limit=limit, start=start_unix, end=end_unix)
+        market_trades = res['trades'] + market_trades
+
+        if len(res['trades']) < limit:
+            break
+
+        start_time = datetime.datetime.fromisoformat(max(res['trades'], key=lambda x: x['time'])['time'])
+        
+    return market_trades
+
+def fetch_market_trade_candles(client: RESTClient, product_id: str, start_time: datetime.datetime, end_time: datetime.datetime, limit: int = CANDLES_LIMIT_MAX):
+    candles = get_asset_candles(client, product_id=product_id, granularity=Granularity.ONE_MINUTE, start=start_time, end=end_time, limit=limit)
     return candles
 
 def main():
     client = get_client()
 
-    try:
-        candles = utils.get_dict_data_from_file(utils.get_path_from_log_dir("candles.json"))["candles"]
-
-        for candle in candles:
-            date: str = utils.unix_to_date_string(int(candle["start"]))
-            high: str = candle["high"]
-            weekday: str = utils.get_weekday(datetime.datetime.fromtimestamp(int(candle["start"])))
-            print(f"{date} {weekday}: {high}")
-    except Exception as _:
-        print("Failed to read data from file: " + utils.get_path_from_log_dir("candles.json"))
-
 if __name__=="__main__":
     main()
+    pass
